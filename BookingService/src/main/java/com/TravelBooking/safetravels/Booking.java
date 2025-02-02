d188e64b2fed9e88c3d165bf72d421169af7da22
Janindu pramod <Janindupramod@gmail.com>
2025-02-02 17:49:13 +0530

    testing swagger

--- BookingService/src/main/java/com/TravelBooking/safetravels/Booking.java
+++ BookingService/src/main/java/com/TravelBooking/safetravels/Booking.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Booking {
 
+	// Testing the Swagger
+
 	public static void main(String[] args) {
 		SpringApplication.run(Booking.class, args);
 	}
